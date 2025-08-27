dfa2800260eceb4bb6c07dc6a0b3badd410efa5d
Arya <aryaaranjann@gmail.com>
2025-08-27 21:11:53 +0530

    Time: 0 ms (100%), Space: 41.8 MB (93.7%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,20 +1,20 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k = 0; // Pointer for the next non-val element
+        int count = 0; // Pointer for the next non-val element
         
         // Iterate through the array with a second pointer 'i'
         for (int i = 0; i < nums.length; i++) {
             // If the current element is not the value to remove,
-            // move it to the k-th position.
+            // move it to the count-th position.
             if (nums[i] != val) {
-                nums[k] = nums[i];
-                // Increment k to point to the next available slot.
-                k++;
+                nums[count] = nums[i];
+                // Increment count to point to the next available slot.
+                count++;
             }
         }
         
-        // k now holds the count of elements not equal to val.
-        return k;
+        // count now holds the count of elements not equal to val.
+        return count;
     }
 }
     
